1f6c3e999896a9f1a329bec463ce99c6ba156e59
David <pbnjgod@gmail.com>
2022-01-06 13:18:31 +0100

    added pixoCharacter and Setup Input
    added content,
     test level,
    character,
     character class

---
+++ Source/ActionRogueLike2/ActionRogueLike2.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ActionRogueLike2.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ActionRogueLike2, "ActionRogueLike2" );
---
+++ Source/ActionRogueLike2/ActionRogueLike2GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionRogueLike2GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROGUELIKE2_API AActionRogueLike2GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
